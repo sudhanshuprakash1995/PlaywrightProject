67749fc1c22bdfb1c3744e0b99bf4dfd5161f29f
sudhanshuprakash1995 <sudhanshuprakash@live.in>
2026-05-26 10:49:40 +0530

    removed only from the existing api test

--- tests/WebApiTesting.spec.js
+++ tests/WebApiTesting.spec.js
@@ -3,6 +3,15 @@ const dataPayload = {
     userEmail: "sp333@gmail.com",
     userPassword: "Sp@123456"
 }
+const orderPayload = {
+    "orders": [
+        {
+            "country": "India",
+            "productOrderedId": "6960ea76c941646b7a8b3dd5"
+        }
+    ]
+}
+let orderId
 let token
 
 test.beforeAll( async () => {
@@ -12,15 +21,31 @@ test.beforeAll( async () => {
     // console.log('Json response: ', responseBody);
     await expect(response.ok()).toBeTruthy();
      token = responseBody.token;
+
+     const orderResponse = await apiContext.post('https://rahulshettyacademy.com/api/ecom/order/create-order',{
+        data: orderPayload,
+        headers:{
+            'Authorization':token,
+            'Content-Type':'application/json'
+        }
+     })
+        const orderResponseBody = await orderResponse.json()
+        console.log('Order response: ', orderResponseBody);
+        orderId = orderResponseBody['orders'][0];
+        console.log('Order ID: ', orderId);
     await apiContext.dispose();
 })
 
-test.only('Bypassing Login', async ({page}) => {
+test('Bypassing Login', async ({page}) => {
     // await page.goto('https://google.com')
     await page.addInitScript(value => {
         window.localStorage.setItem('token', value)
 }, token)
 
 await page.goto('https://rahulshettyacademy.com/client/#/dashboard')
-await page.waitForTimeout(5000)
+await page.waitForTimeout(2000)
+await page.goto('https://rahulshettyacademy.com/client/#/dashboard/myorders')
+await page.pause()
+await page.goto(`https://rahulshettyacademy.com/client/#/dashboard/order-details/${orderId}`)
+await page.pause()
 })
