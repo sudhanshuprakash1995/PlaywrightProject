ee0f462c873be9fbb7d82a76b81893ac8884eb74
sudhanshuprakash1995 <sudhanshuprakash@live.in>
2026-05-26 10:55:21 +0530

    enabled headless in the config file

--- playwright.config.js
+++ playwright.config.js
@@ -37,7 +37,7 @@ export default defineConfig({
     trace: 'retain-on-failure',
     screenshot: 'only-on-failure',
     browserName: 'chromium',
-    headless: false,
+    headless: true,
   },
 
   /* Configure projects for major browsers */
